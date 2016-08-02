7444636aef12864e76fe49e2f8aab6084f33d832
GarthPearce <g.pearce@unsw.edu.au>
2016-08-02 15:40:50 +1000

    include sift results containers

--- +sift/STtoJ1.m
+++
@@ -1,3 +0,0 @@
-function J1=STtoJ1(ST)
-
-J1=ST(1,:)+ST(2,:)+ST(3,:);
--- +sift/dehomogenise.m
+++ +sift/dehomogenise.m
@@ -2,11 +2,16 @@ function result = dehomogenise(strain,deltaT,MME,nMatrixIP,nFibreIP,nArrays,nAng
 %SIFT_user(strain,deltaT,MME,nMatrixIP,nFibreIP,nArrays,nAngles,crit,results
 %dehomogenise Summary of this function goes here
 %   Detailed explanation goes here
+    load('rot_t.mat');
+    for iAngle = 1:nAngles
+        rotStrain(:,iAngle) = rot_t(:,:,iAngle)*strain(:)
+    end
+    
     modStrain = zeros(6,max(nMatrixIP,nFibreIP),nArrays,nAngles);
     for iPosition = 1:nMatrixIP
         for iArray = 1:nArrays
             for iAngle = 1:nAngles
-                modStrain(:,iPosition,iArray,iAngle) = MME(:,1:6,iPosition,iArray)*strain;
+                modStrain(:,iPosition,iArray,iAngle) = MME(:,1:6,iPosition,iArray)*rotStrain(:,iAngle);
                 modStrain(:,iPosition,iArray,iAngle) = modStrain(:,iPosition,iArray,iAngle)+deltaT*MME(:,7,iPosition,iArray);
                 matrixDil(iPosition,iArray,iAngle) = sift.eDil(modStrain(:,iPosition, iArray, iAngle));
                 matrixDis(iPosition,iArray,iAngle) = sift.eDis(modStrain(:,iPosition, iArray, iAngle));
@@ -16,9 +21,9 @@ function result = dehomogenise(strain,deltaT,MME,nMatrixIP,nFibreIP,nArrays,nAng
     
     %Find maximum invariants and assign them to output
     %%Create containers for results
-    result = siftResult;
-    invariant = siftInvariant;
-    location = siftLocation;
+    result = sift.result;
+    invariant = sift.invariant;
+    location = sift.location;
     
     %Find maximum invariants and store the index in column format
     [invariant.matrixDil , matrixDilLocInd] = max( matrixDil(:) );
@@ -26,9 +31,9 @@ function result = dehomogenise(strain,deltaT,MME,nMatrixIP,nFibreIP,nArrays,nAng
     
     %Store the critical location in a location index
     [point,array,angle] = ind2sub([max(nMatrixIP,nFibreIP),nArrays,nAngles],matrixDilLocInd);
-    location.matrixDil = siftLocationIndex([point,array,angle]);
+    location.matrixDil = sift.locationIndex([point,array,angle]);
     [point,array,angle] = ind2sub([max(nMatrixIP,nFibreIP),nArrays,nAngles],matrixDisLocInd);
-    location.matrixDis = siftLocationIndex([point,array,angle]);
+    location.matrixDis = sift.locationIndex([point,array,angle]);
     
     %Construct result
     result.invariant = invariant;
